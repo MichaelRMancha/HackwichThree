8ccb807ef43ccdfed16ecbdc09d5c7956f329918
CM Student <cmstudent@uhwo-acm-15.local>
2018-02-05 10:50:57 -1000

    Added a bunch of code. Changed the label programmatically to "About". Added glyphs from outside source to use for tabs.

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var firstLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//set firstLabel to the string "About"
+        
+self.firstLabel.text = "About"
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
